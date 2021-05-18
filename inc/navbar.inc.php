c0fbc33d69823751f9b04167678730363c17adcf
Ibrahim ADLANI <ibrahim@adlani.com>
2021-05-18 20:11:13 +0200

    connexion et inscription page

---
+++ inc/navbar.inc.php
@@ -0,0 +1,38 @@
+<nav class="navbar navbar-expand-lg navbar-light border-bottom">
+            <div class="container">
+                <a class="navbar-brand d-lg-none d-block" href="#" style="width:258px;"><img src="img/cityQuest.svg" alt="" height="50"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse  justify-content-between text-center" id="navbarNav">
+                <a class="navbar-brand d-none d-lg-block" href="#" style="width:258px;"><img src="img/cityQuest.svg" alt="" height="50"></a>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                    <a class="nav-link px-3" href="index.php">Carte</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link px-3" href="dashboard.php">Offres</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link px-3" href="offres.php">Contact</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav d-none d-lg-flex">
+                    <li class="nav-item border-start border-end">
+                    <a class="nav-link px-3" href="connexion.php"><i class="fas fa-sign-in-alt"></i> Connexion</a>
+                    </li>
+                    <li class="nav-item border-end">
+                    <a class="nav-link px-3" href="inscription.php"><i class="far fa-user"></i> Inscription</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav d-lg-none">
+                    <li class="nav-item">
+                    <a class="nav-link px-3 border-top" href="connexion.php"><i class="fas fa-sign-in-alt"></i> Connexion</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link px-3 border-top" href="connexion.php"><i class="far fa-user"></i> Inscription</a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </nav>
